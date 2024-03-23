4be109a32eefa05fafc41cc86c51e1c8032ee637
mroley1 <64291507+mroley1@users.noreply.github.com>
2024-03-23 02:10:00 -0400

    add participant endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use mysql::*;
 use mysql::prelude::*;
+use rocket::http::Status;
 use rocket::response::status;
+use rocket::serde::json::Json;
 
 mod tables;
 
@@ -30,11 +32,31 @@ fn index() -> status::Accepted<String> {
         },
     ).unwrap();
     
-    status::Accepted(format!("{}, {}", select[1].id, select[1].name))
+    status::Accepted(format!("{}, {}", select[3].id, select[3].name))
 }
+
+#[get("/participant/<id>")]
+    fn get_participant(id: i32) -> Json<tables::Participants> {
+        let response = db_connect().query_map(
+            format!("SELECT * FROM participants WHERE id = {} LIMIT 1;", id),
+            |(id, name)| {
+                tables::Participants{ id, name}
+            }
+        ).unwrap();
+        Json(response[0].clone())
+    }
+
+#[post("/participant/<name>")]
+fn new_participant(name: String) -> Status {
+    match db_connect().query_drop(format!("INSERT INTO participants(name) VALUES ('{}');", name)) {
+        Ok(()) => Status::Ok,
+        Err(_) => Status::InternalServerError,
+    }
+}
+
 
 #[launch]
 fn rocket() -> _ {
     
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, new_participant, get_participant])
 }
--- src/tables.rs
+++ src/tables.rs
@@ -1,6 +1,7 @@
+use rocket::serde::Serialize;
 
-
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Serialize, Clone)]
+#[serde(crate = "rocket::serde")]
 pub struct Availability {
     pub id: i32,
     pub participant_id: i32,
@@ -9,26 +10,30 @@ pub struct Availability {
     pub end: i32,
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Serialize, Clone)]
+#[serde(crate = "rocket::serde")]
 pub struct Columns {
     pub id: i32,
     pub name: String,
     pub length: i32,
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Serialize, Clone)]
+#[serde(crate = "rocket::serde")]
 pub struct Ownership {
     pub meeting_id: i32,
     pub participant_id: i32,
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Serialize, Clone)]
+#[serde(crate = "rocket::serde")]
 pub struct Participants {
     pub id: i32,
     pub name: String,
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Serialize, Clone)]
+#[serde(crate = "rocket::serde")]
 pub struct Participation {
     pub meeting_id: i32,
     pub participant_id: i32,
