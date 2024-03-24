8ef518ab08368f65156e1d9c7d68206325a34bdc
mroley1 <64291507+mroley1@users.noreply.github.com>
2024-03-24 18:46:29 -0400

    add coloring to chart

--- ui/src/app/user/[id]/column.tsx
+++ ui/src/app/user/[id]/column.tsx
@@ -1,29 +1,45 @@
 "use client";
 
 import styles from "./page.module.scss"
-import { availabilityPoint, dayReference } from "./page";
+import { AvailabilityPoint, dayReference } from "./page";
+
+interface Slot {
+    timeStart: number,
+    priority: number
+}
 
 interface props {
     dayIndex: number
-    day: availabilityPoint[],
-    setter: (data: availabilityPoint[])=>void
+    day: AvailabilityPoint[],
+    setter: (data: AvailabilityPoint[])=>void
 }
 export default function Column({dayIndex, day, setter}: props) {
     
-    let slots = []
-    
-    for (var i=0; i<1440; i=i+15) {
-        slots.push(<TimeSlot key={i} timeStart={i}></TimeSlot>)
+    let slots: Slot[] = []
+    const priority = 0
+    for (var timeStart=0; timeStart<1440; timeStart=timeStart+15) {
+        slots.push({timeStart, priority})
     }
+    
+    day.forEach((availabilityPoint: AvailabilityPoint)=>{
+        slots.filter((slot)=>{
+            if (( slot.priority == 0 || slot.priority > availabilityPoint.p) && availabilityPoint.s <= slot.timeStart && availabilityPoint.e > slot.timeStart) {
+                slot.priority = availabilityPoint.p
+            }
+            return slot;
+        })
+    });
+    
+    const slotElements = slots.map(({timeStart, priority}: Slot) => <TimeSlot key={timeStart} priority={priority} timeStart={timeStart}></TimeSlot>)
     
     return (
       <div data-dayIndex={dayReference[dayIndex]} className={styles.column}>
-        {slots}
+        {slotElements}
       </div>
     );
 }
 
-function TimeSlot({timeStart}: {timeStart: number}) {
+function TimeSlot({timeStart, priority}: Slot) {
     const time = (() => {
         let i = Math.floor(timeStart/60)
         let post = "AM"
@@ -35,6 +51,6 @@ function TimeSlot({timeStart}: {timeStart: number}) {
         return i + post;
     })()
     return (
-        <div data-hour={time}></div>
+        <div data-hour={time} data-priority={priority} data-debug={timeStart}></div>
     )
 }
--- ui/src/app/user/[id]/page.tsx
+++ ui/src/app/user/[id]/page.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import styles from "./page.module.scss"
 import Column from "./column";
 
-export interface availabilityPoint {
+export interface AvailabilityPoint {
     p:number, s:number, e:number
 }
 
@@ -20,20 +20,21 @@ export const dayReference = [
 
 export default function Page({params}: {params: {id: number}}) {
     
-    let data: availabilityPoint[][] = [
+    // min: 0 max: 1440
+    let data: AvailabilityPoint[][] = [
         [],
-        [{p:2, s:30, e:255}],
-        [{p:2, s:30, e:355}],
-        [{p:3, s:30, e:455}, {p:4, s:50, e:70}],
-        [{p:1, s:300, e:600}, {p:4, s:50, e:70}],
+        [{p:2, s:100, e:255}],
+        [{p:2, s:300, e:1255}],
+        [{p:3, s:300, e:455}, {p:4, s:70, e:200}],
+        [{p:2, s:1000, e:1440}, {p:4, s:0, e:70}, {p:1, s:1100, e:1200}],
         [],
         [{p:5, s:30, e:31}],
     ]
     
     const [dayData, setDayDate] = useState(data);
     
-    function setCol(index: number, data: availabilityPoint[]) {
-        const tmp: availabilityPoint[][] = dayData;
+    function setCol(index: number, data: AvailabilityPoint[]) {
+        const tmp: AvailabilityPoint[][] = dayData;
         tmp[index] = data;
         setDayDate(tmp);
     }
@@ -41,7 +42,7 @@ export default function Page({params}: {params: {id: number}}) {
     return (
       <div>
         <div className={styles.table}>
-            {dayData.map((day, index) => <Column key={index} dayIndex={index} day={day} setter={(data: availabilityPoint[])=>{setCol(index, data)}}></Column>)}
+            {dayData.map((day, index) => <Column key={index} dayIndex={index} day={day} setter={(data: AvailabilityPoint[])=>{setCol(index, data)}}></Column>)}
         </div>
       </div>
     );
